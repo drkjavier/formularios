1f37861bfac37a50e311cae1574e989eb7bc24fd
Dereck Curtis <drkjavier@gmail.com>
2019-03-17 02:12:22 -0600

    Video 179-Cambios

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -24,7 +24,7 @@ export class DataComponent implements OnInit {
         'nombre': new FormControl(this.usuario.nombrecompleto.nombre, [ Validators.required, Validators.minLength(3) ]),
         'apellido': new FormControl(this.usuario.nombrecompleto.apellido, [ Validators.required, Validators.minLength(3) ])
       }),
-      'correo': new FormControl(this.usuario.correo, [  Validators.required,
+      'correo': new FormControl(this.usuario.correo, [  Validators.required,  
                                                         Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ]),
       'pasatiempos': new FormArray([
         new FormControl('Correr', Validators.required)
@@ -36,6 +36,14 @@ export class DataComponent implements OnInit {
 
   guardarCambios () {
     console.log(this.forma);
+
+    this.forma.reset({
+      nombrecompleto: {
+        nombre: "",
+        apellido: ""
+      },
+      correo: ""
+    });
   }
 
   guardarPasatiempos() {
